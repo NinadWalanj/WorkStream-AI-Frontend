51d4326fee765ebe391353056bea341e30063456
Ninad Walanj <ninadwalanj@gmail.com>
2025-06-02 18:32:32 -0400

    callback.jsx removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import ProtectedRoute from "./ProtectedRoute";
-import Callback from "./Callback";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/auth/github/callback" element={<Callback />} />
         <Route
           path="/"
           element={
