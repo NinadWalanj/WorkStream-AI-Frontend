2dfd9ff7e9dc3ce027e1fdd816b987bb83eaf2ea
Ninad Walanj <ninadwalanj@gmail.com>
2025-06-02 17:04:02 -0400

    include callback.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import ProtectedRoute from "./ProtectedRoute";
+import Callback from "./Callback";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/auth/github/callback" element={<Callback />} />
         <Route
           path="/"
           element={
